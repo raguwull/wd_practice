46f48d45405e434cf8aa78b2f6b23a3561bac4ff
Ragul B <ragulb8825@gmail.com>
2024-04-02 19:04:25 +0530

    Write code to understand API handing using Axios

---
+++ LearnAxios/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const type = req.body.type;
+    const response = axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}`
+    );
+    var data = (await response).data;
+    res.send(data[0].activity);
+  } catch (error) {
+    console.error("Failed to make request :", error.message);
+    res.status(500).send("Failed to fetch activity");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
