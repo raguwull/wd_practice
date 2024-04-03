3ac491a9f08828bbba2af159b70a944a5e8554a3
Ragul B <ragulb8825@gmail.com>
2024-04-03 21:11:43 +0530

    Update code for Posty Express

--- PostgresqlExpress/server.js
+++ PostgresqlExpress/server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import pkg from "pg"; // Import the entire pg package
 
 dotenv.config();
 
+const { Client } = pkg; // Destructure the Client object from the pg package
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Listening on port ${PORT}`));
+const db = new Client({
+    user: "ragulb",
+    host: "localhost",
+    database: "myloginapp",
+    password: "password",
+    port: 5432
+});
+
+db.connect();
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
